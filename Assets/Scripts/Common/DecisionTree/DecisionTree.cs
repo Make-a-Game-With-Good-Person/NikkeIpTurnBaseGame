9b6b833ebd062c90471904fa251d5cd5650d6f79
Razack <57121998+wsxed1230@users.noreply.github.com>
2024-12-26 14:07:13 +0900

    1226 결정트리 깃허브에서 찾아온것

---
+++ Assets/Scripts/Common/DecisionTree/DecisionTree.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DecisionTree
+{
+    public abstract class DecisionTreeNode
+    {
+        public abstract DecisionTreeNode MakeDecision(); 
+    }
+
+    public abstract class FinalDecision : DecisionTreeNode
+    {
+        public override DecisionTreeNode MakeDecision()
+        {
+            return this;
+        }
+
+        public abstract void Execute();
+    }
+
+    public abstract class Decision : DecisionTreeNode
+    {
+        protected DecisionTreeNode _trueNode;
+        protected DecisionTreeNode _falseNode;
+
+        public Decision(DecisionTreeNode trueNode, DecisionTreeNode falseNode)
+        {
+            _trueNode = trueNode;
+            _falseNode = falseNode;
+        }
+
+        abstract public DecisionTreeNode GetBranch();
+
+        public override DecisionTreeNode MakeDecision()
+        {
+            DecisionTreeNode branch = GetBranch();
+            return branch.MakeDecision();
+        }
+    }
+}
